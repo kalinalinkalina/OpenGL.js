39b48c2687b1030aef0610e21a0d49f9b8c506b9
Kalina Borkiewicz <borkiew1@illinois.edu>
2012-05-21 10:33:24 -0500

    cleaned up

--- glu.js
+++
@@ -1,5 +0,0 @@
-gluLookAt = function(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ) {
-	$W.camera.setPosition(eyeX, eyeY, eyeZ);
-	$W.camera.setTarget(centerX, centerY, centerZ);
-	$W.camera.up = $V([upX, upY, upZ]);
-}
